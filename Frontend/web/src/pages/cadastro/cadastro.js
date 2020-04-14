066ed97678d732620780ac28800172cf06730add
gaqp <gaqp@cin.ufpe.br>
2020-04-14 18:45:49 -0300

    Pequenas alterações

---
+++ Frontend/web/src/pages/cadastro/cadastro.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+export default class Cadastro extends Component{
+    render(){
+        
+    }
+}
--- Frontend/web/src/pages/home/header.js
+++ Frontend/web/src/pages/home/header.js
@@ -11,7 +11,7 @@ const Header = () => {
             <Link to="#project">Projeto</Link>
             <Link to="#contato">Contato</Link>
 
-            <button id="sign in">Fazer Login</button>
+            <button id="sign in">Entrar</button>
         </header>
 
     );
