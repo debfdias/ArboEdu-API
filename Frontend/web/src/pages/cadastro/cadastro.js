ceecbf50bf0d6271a07a0a2d185ce38f19fb1a38
gaqp <gaqp@cin.ufpe.br>
2020-04-16 14:51:46 -0300

    Cadastro

--- Frontend/web/src/pages/cadastro/cadastro.js
+++ Frontend/web/src/pages/cadastro/cadastro.js
@@ -1,54 +1,138 @@
 import React, { Component } from 'react';
+import api from '../../services/api.js';
 
 export default class Cadastro extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 'Escolha uma opção antes de continuar o cadastro',
+            formData:{}
+        };
+        this.cadastro = this.cadastro.bind(this);
+    };
     render() {
         return (
             <div>
                 <div id="seletor">
-                    <select id="Tipo" onChange={this.cadastro()}>
+                    <select id="Tipo" onChange={this.cadastro}>
+                        <option value=""></option>
                         <option value="Administrador">Administrador</option>
                         <option value="Jovem ACE">Jovem ACE</option>
                         <option value="Pesquisador">Pesquisador</option>
                         <option value="Professor">Professor</option>
                         <option value="Profissional de Educação">Profissional de Educação</option>
                     </select>
+                    {this.state.value}
                 </div>
             </div>
         );
     }
     cadastro(opção) {
-        console.log(opção);
+        switch (opção.target.value) {
+            case ("Administrador"):
+                this.setState({ value: <this.admin /> });
+                break;
+            case ("Jovem ACE"):
+                this.setState({ value: <this.jovemAce /> });
+                break;
+            case (""):
+                this.setState({ value: 'Escolha uma opção antes de continuar o cadastro' });
+                break;
+            default:
+                this.setState({ value: <this.diretorProfessorPatrono /> })
+
+
+        }
     };
-    generico() {
+    enviarDados(){
+        console.log(api.post("/user", {
+            "name": "João da Silva",
+            "phone": "8512345678",
+            "email": "teste1@uol.com",
+            "cpf": "4328938439",
+            "birthday": "1997-06-10",
+            "password": "123456",
+            "role": "pesquisador",
+            "extra": {
+                "institution": "FIOCRUZ"
+            }
+        }))
+    };
+    admin() {
         return (
-                <div>
-                <input type="text" id="nome" placeholder="Nome completo" /><br />
-                <input type="text" id="cpf" placeholder="Ex. 123.456.789-10" required pattern="\d{3}.?\d{3}.?\d{3}-?\d{2}" /><br />
-                <input type="text" id="cep" placeholder="Ex. 12345-678" required pattern="\d{5}-?\d{3}" /><br />
-                <input type="date" id="dataNascimento" /><br />
-                <input type="email" id="email" placeholder="Ex. contato@arboedu.com.br" />
-                <input type="password" id="Senha" placeholder="********" />
-                <input type="password" id="SenhaConfirmacao" placeholder="********" />
-                </div>
+            <div>
+                <form id="adminForm">
+                    <label htmlFor="nome">Nome</label>
+                    <input required type="text" id="nome" placeholder="Nome completo" /><br />
+                    <label htmlFor="cpf">CPF</label>
+                    <input required type="text" id="cpf" placeholder="Ex. 123.456.789-10" pattern="\d{3}\.?\d{3}\.?\d{3}-?\d{2}" /><br />
+                    <label htmlFor="cep">CEP</label>
+                    <input required type="text" id="cep" placeholder="Ex. 12345-678" pattern="\d{5}-?\d{3}" /><br />
+                    <label htmlFor="dataNascimento">Data de nascimento</label>
+                    <input required type="date" id="dataNascimento" /><br />
+                    <label htmlFor="email">E-mail</label>
+                    <input required type="email" id="email" placeholder="Ex. contato@arboedu.com.br" /><br />
+                    <label htmlFor="senha">Senha</label>
+                    <input required type="password" id="senha" placeholder="********" /><br />
+                    <label htmlFor="senhaConfirmacao">Repetir senha</label>
+                    <input required type="password" id="senhaConfirmacao" placeholder="********" /><br />
+                    <input required type="submit" value="Enviar" /><br />
+                </form>
+            </div>
         );
     };
-    jovemAce(){
-        return(
+    jovemAce() {
+        return (
             <div>
-            this.generico(),
-            <input type="text" id="DS" placeholder="Distrito Sanitário"/>
-            <input type="text" id="EDS" placeholder="Escola que supervisiona no distrito sanitário"/>
+                <form>
+                    <label htmlFor="nome">Nome</label>
+                    <input required type="text" id="nome" placeholder="Nome completo" /><br />
+                    <label htmlFor="cpf">CPF</label>
+                    <input required type="text" id="cpf" placeholder="Ex. 123.456.789-10" pattern="\d{3}\.?\d{3}\.?\d{3}-?\d{2}" /><br />
+                    <label htmlFor="cep">CEP</label>
+                    <input required type="text" id="cep" placeholder="Ex. 12345-678" pattern="\d{5}-?\d{3}" /><br />
+                    <label htmlFor="dataNascimento">Data de nascimento</label>
+                    <input required type="date" id="dataNascimento" /><br />
+                    <label htmlFor="email">E-mail</label>
+                    <input required type="email" id="email" placeholder="Ex. contato@arboedu.com.br" /><br />
+                    <label htmlFor="senha">Senha</label>
+                    <input required type="password" id="senha" placeholder="********" /><br />
+                    <label htmlFor="senhaConfirmacao">Repetir senha</label>
+                    <input required type="password" id="senhaConfirmacao" placeholder="********" /><br />
+                    <label htmlFor="DS">Distrito Sanitário</label>
+                    <input required type="text" id="DS" placeholder="Distrito Sanitário" /><br />
+                    <label htmlFor="EDS">Escola que supervisiona no distrito sanitário</label>
+                    <input required type="text" id="EDS" placeholder="Escola que supervisiona no distrito sanitário" /><br />
+                    <input required type="submit" value="Enviar" /><br />
+                </form>
             </div>
         );
     }
-    diretorProfessorPatrono(){
-        return(
+    diretorProfessorPatrono() {
+        return (
             <div>
-            this.generico(),
-            <input type="text" id="disciplina" placeholder="Disciplina"/>
-            <input type="text" id="Escola" placeholder="Escola"/>
+                <form>
+                    <label htmlFor="nome">Nome</label>
+                    <input required type="text" id="nome" placeholder="Nome completo" /><br />
+                    <label htmlFor="cpf">CPF</label>
+                    <input required type="text" id="cpf" placeholder="Ex. 123.456.789-10" pattern="\d{3}\.?\d{3}\.?\d{3}-?\d{2}" /><br />
+                    <label htmlFor="cep">CEP</label>
+                    <input required type="text" id="cep" placeholder="Ex. 12345-678" pattern="\d{5}-?\d{3}" /><br />
+                    <label htmlFor="dataNascimento">Data de nascimento</label>
+                    <input required type="date" id="dataNascimento" /><br />
+                    <label htmlFor="email">E-mail</label>
+                    <input required type="email" id="email" placeholder="Ex. contato@arboedu.com.br" /><br />
+                    <label htmlFor="senha">Senha</label>
+                    <input required type="password" id="senha" placeholder="********" /><br />
+                    <label htmlFor="senhaConfirmacao">Repetir senha</label>
+                    <input required type="password" id="senhaConfirmacao" placeholder="********" /><br />
+                    <label htmlFor="disciplina">Disciplina</label>
+                    <input required type="text" id="disciplina" placeholder="Disciplina" /><br />
+                    <label htmlFor="escola">Escola</label>
+                    <input required type="text" id="escola" placeholder="Escola" /><br />
+                    <input required type="submit" value="Enviar" /><br />
+                </form>
             </div>
         );
     }
-    
 }
