42c8393c6f88bf5be0b3d37b1199a92e972975ad
IzabellaPcn <43002486+IzabellaPcn@users.noreply.github.com>
2020-04-16 15:56:51 -0300

    Correção de erro no header

--- Frontend/web/src/pages/home/header.js
+++ Frontend/web/src/pages/home/header.js
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 
 import "./header.css";
 
-const Header = () => 
-    return(
-        <header id='home-header'>
-            <Link to="#home">Home</Link>
-            <Link to="#about">Sobre nós</Link>
-            <Link to="#project">Projeto</Link>
-            <Link to="#contato">Contato</Link>
-            <Link to="/cadastro"><button id="sign in">Entrar</button></Link>
-        </header>
+const Header = () => {
+    return ( <
+        header id = 'home-header' >
+        <
+        Link to = "#home" > Home < /Link> <
+        Link to = "#about" > Sobre nós < /Link> <
+        Link to = "#project" > Projeto < /Link> <
+        Link to = "#contato" > Contato < /Link> <
+        Link to = "/cadastro" > < button id = "sign in" > Entrar < /button></Link >
+        <
+        /header>
 
     );
 }
