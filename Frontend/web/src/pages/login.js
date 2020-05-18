b02808e666668c6d0c8dc2ee3818aecfd9b603a7
gaqp <gaqp@cin.ufpe.br>
2020-05-18 11:37:41 -0300

    Recuperação de senha Ok

--- Frontend/web/src/pages/login.js
+++ Frontend/web/src/pages/login.js
@@ -9,45 +9,61 @@ import Button from 'react-bootstrap/Button'
 export default class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {email: '', password: ''};
-    
+        this.state = { email: '', password: ''};
+        this.login = true;
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-      }
-      handleChange(event){
-        this.setState({ ...this.state,[event.target.type] : event.target.value});
-      }
-      handleSubmit(event){
-        if(this.state.email  !== '' && this.state.password !==''){
-            api.post("/user/authenticate",this.state).then((response)=>{
+        this.forgotPassword = this.forgotPassword.bind(this);
+    }
+    handleChange(event) {
+        this.setState({ ...this.state, [event.target.type]: event.target.value });
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        if (this.state.email !== '' && (this.state.password !== '' || !this.login )) {
+            if(!this.login){
+                api.post('/user/passwordRecovery', {email: this.state.email}).then((response)=>{
+                    console.log(response);
+                }).catch((error)=>{
+                    console.log(error.response);
+                });
+            }
+            api.post('/signin', {email: this.state.email, password : this.state.password}).then((response) => {
                 console.log(response);
-            }).catch((error)=>{
-                console.log(error.response.data);
+            }).catch((error) => {
+                console.log(error);
             });
         }
     }
+    forgotPassword(event){
+        this.login = !this.login;
+        this.forceUpdate();
+    }
     render() {
 
-        
+
         return (
             <React.Fragment>
                 <Header />
-                <Form onSubmit={this.handleSubmit} action="#">
+                <Form onSubmit={this.handleSubmit} >
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>E-mail</Form.Label>
                         <Form.Control type="email" placeholder="Digite seu e-mail" value={this.state.email} onChange={this.handleChange} />
                     </Form.Group>
 
+                    {this.login && <div>
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type="password" placeholder="Insira a sua senha" value={this.state.password} onChange={this.handleChange} />
+                        </Form.Group>
+                    </div>}
+                    <Button variant="primary" type="submit"> {this.login ? "Entrar" : "Enviar"}</Button>
+                    <Button variant="secondary" onClick={this.forgotPassword} >{this.login ? "Esqueceu sua senha?" :"Voltar a tela de Login"} </Button>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Senha</Form.Label>
-                        <Form.Control type="password" placeholder="Insira a sua senha" value={this.state.password} onChange={this.handleChange} />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">Entrar</Button>
                 </Form>
             </React.Fragment>
         );
     }
-   
+
 }
 
