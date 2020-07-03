fc2e952dcfbf23bb0323f2964c06ac4836b70586
Italo Soares <irs@cin.ufpe.br>
2020-07-02 22:56:49 -0300

    [FEATURE] Lógica de update para tabelas relacionadas a Quiz, Questions e Quiz_Questions

--- Backend/src/app/controllers/Quizzes.js
+++ Backend/src/app/controllers/Quizzes.js
@@ -34,9 +34,40 @@ class QuestionsController {
   async update(req, res) {
     try {
       Quiz.findByPk(req.params.id).then(questionToBeUpdated=>{
-          questionToBeUpdated.update(req.body).then(result=>{
-              console.log(result);
-              res.status(200).json(result);
+          questionToBeUpdated.update(req.body).then(quizzes=>{
+            Quiz_Questions.findAll({where:{
+              QuizId: req.params.id
+            }}).then(found=>{
+              //====================================================================
+              //Para atualizar tabelas relacionadas com adição
+              var questionsOnDatabase=[]
+              found.forEach(element=>{
+                questionsOnDatabase.push(element.dataValues.QuestionId)
+              })
+              req.body.list_questions.forEach(element=>{
+                if(!questionsOnDatabase.includes(element)){
+                  console.log(element);
+                  Quiz_Questions.create({
+                    QuizId: req.params.id,
+                    QuestionId: element
+                  }).then().catch(err=>{res.status(500).json("Não foi possível acrescentar uma ou mais questões.", err)})
+                }
+              })
+              //====================================================================
+              //Para atualizar tabelas relacionadas com exclusão
+              var questionsOnRequisition=[]
+              req.body.list_questions.forEach(element=>{
+                questionsOnRequisition.push(element)
+              })
+              found.forEach(element=>{
+                if(!questionsOnRequisition.includes(element.dataValues.QuestionId)){
+                  console.log(element.dataValues.QuestionId);
+                  element.destroy().then().catch(err=>{res.status(500).json("Erro ao remover uma ou mais questões.", err)})
+                }
+              })
+            })
+            
+            res.status(200).json("OK");
           });
       });
     } catch (err) {
@@ -46,8 +77,8 @@ class QuestionsController {
 
   async destroy(req, res) {
     try {
-      Quiz.findByPk(req.params.id).then(questionToBeDestroyd=>{
-        questionToBeDestroyd.destroy().then(result=>{
+      Quiz.findByPk(req.params.id).then(questionToBeDestroyed=>{
+        questionToBeDestroyed.destroy().then(result=>{
             res.status(200).json(result);
         })
       })
